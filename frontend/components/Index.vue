303ba9e5c5059b5228edd2e9600c642a2a32d76b
vlad <vladviolentiy@gmail.com>
2023-09-29 15:49:09 +0300

    fix localization on frontend

--- frontend/components/Index.vue
+++ frontend/components/Index.vue
@@ -1,11 +1,16 @@
 <template>
-  <h4>Index</h4>
+  <h4>{{Localization.welcome}}</h4>
 </template>
 
 <script lang="ts">
 import {defineComponent} from "vue";
+import {mapState} from "pinia";
+import {appStore} from "@/stores/AppStore";
 export default defineComponent({
-  name: "IndexPage"
+  name: "IndexPage",
+  computed: {
+    ...mapState(appStore, ['Localization']),
+  },
 })
 </script>
 
