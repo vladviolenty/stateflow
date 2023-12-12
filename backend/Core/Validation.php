28c59ed66d485d83c2814c910917a89dea0d0c52
Vlad Violentiy <vladviolentiy@gmail.com>
2023-12-12 19:58:09 +0300

    Fix validation RSA key in nonstandart format

--- backend/Core/Validation.php
+++ backend/Core/Validation.php
@@ -7,6 +7,13 @@ use VladViolentiy\VivaFramework\Exceptions\ValidationException;
 class Validation
 {
     public static function RSAPublicKey(string $keyInput):void{
+        if(!str_starts_with("---",$keyInput)){
+            $keyInput = "
+-----BEGIN PUBLIC KEY-----
+$keyInput
+-----END PUBLIC KEY-----
+";
+        }
         $publicKey = openssl_get_publickey($keyInput);
         if (!$publicKey) {
             throw new ValidationException();
