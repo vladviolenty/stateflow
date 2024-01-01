10fca2200ebfa3df323bf092faa09b79078df246
Vlad Violentiy <vladviolentiy@gmail.com>
2024-01-01 23:39:20 +0300

    update code for creating organization

--- frontend/components/Workflow/Modals/CreateNewOrgModal.vue
+++ frontend/components/Workflow/Modals/CreateNewOrgModal.vue
@@ -18,6 +18,7 @@
       </div>
 
       <button class="btn btn-primary w-100" :disabled="creatingInProcess" @click="createOrg">Создать организацию</button>
+      <p class="m-0 text-center text-danger">{{errorText}}</p>
     </div>
   </div>
 </template>
@@ -28,6 +29,8 @@ import Security from "@/security/Security";
 import Encryption from "@/security/Encryption";
 import WorkflowGateway from "@/gateway/WorkflowGateway";
 import {Offcanvas} from "bootstrap";
+import {mapState} from "pinia";
+import {appStore} from "@/stores/AppStore";
 
 export default defineComponent({
   name: "CreateNewOrgModal",
@@ -38,8 +41,13 @@ export default defineComponent({
       makePublicFLNames: false as boolean,
       creatingInProcess: false as boolean,
 
-      gateway: new WorkflowGateway()
+      errorText:"" as string,
+
+      gateway: new WorkflowGateway(localStorage.getItem("authToken")??"")
     }
+  },
+  computed:{
+    ...mapState(appStore, ['Localization',"DashboardGateway"]),
   },
   methods:{
     showOffcanvas():void{
@@ -51,13 +59,27 @@ export default defineComponent({
       this.creatingInProcess = false;
     },
     async createOrg(){
+      this.creatingInProcess = true;
+
+      if(this.orgName===""){
+        this.errorText = "Имя организации пустое";
+        this.creatingInProcess = false;
+        return;
+      }
+      if(this.genericId===""){
+        this.errorText = "Общий идентификатор пустой";
+        this.creatingInProcess = false;
+        return;
+      }
+
       let iv = Security.getRandom(16);
       let salt = Security.getRandom(16);
       let orgPasswordString = window.btoa(Security.ab2str(Security.getRandom(16)))
       let derivedOrganizationKey = await Encryption.deriveKey(orgPasswordString,salt);
 
-      let userPassword = await Encryption.deriveKey(localStorage.getItem("password")??"",Security.str2ab(localStorage.getItem("salt")??""))
-      let encryptedOrgPasswordByUserPassword = await Encryption.encryptAES(orgPasswordString,userPassword,localStorage.getItem("iv")??"");
+      let userPassword = await Security.getDerivedKey();
+      let userIv = localStorage.getItem("iv")??"";
+      let encryptedOrgPasswordByUserPassword = await Encryption.encryptAES(orgPasswordString,userPassword,userIv);
 
       let rsaKey = await Encryption.generateRSA();
       let basePublic = await Encryption.exportPublicKey(rsaKey.publicKey);
@@ -65,18 +87,30 @@ export default defineComponent({
 
       let encryptedOrgPrivateKey = await Encryption.encryptAESBytes(basePrivate,derivedOrganizationKey,iv);
 
+      let resultName = ""
+      if(this.makePublicFLNames){
+        let currentUserNameInfo = await this.DashboardGateway.getGeneralInfo();
+        if(currentUserNameInfo.success){
+          resultName = await Encryption.decryptAES(currentUserNameInfo.data.fNameEncrypted,userPassword,userIv) + ' '+ await Encryption.decryptAES(currentUserNameInfo.data.lNameEncrypted,userPassword,userIv)
+        }
+      }
+      let ivString = window.btoa(Security.ab2str(iv));
       this.gateway.createNewOrg(
-          window.btoa(Security.ab2str(await Encryption.encryptAESBytes(Security.utf8str2ab(this.orgName),derivedOrganizationKey,iv))),
-          window.btoa(Security.ab2str(await Encryption.encryptAESBytes(Security.utf8str2ab(this.genericId),derivedOrganizationKey,iv))),
-          this.makePublicFLNames,
-          window.btoa(Security.ab2str(iv)),
+          await Encryption.encryptAES(this.orgName,derivedOrganizationKey,ivString),
+          await Encryption.encryptAES(this.genericId,derivedOrganizationKey,ivString),
+          await Encryption.encryptAES(resultName,derivedOrganizationKey,ivString),
+          ivString,
           window.btoa(Security.ab2str(salt)),
           encryptedOrgPasswordByUserPassword,
           basePublic,
-          window.btoa(Security.ab2str(encryptedOrgPrivateKey))
-      ).then(response=>{
-        if(response.success){
+          window.btoa(Security.ab2str(encryptedOrgPrivateKey)),
+          await Encryption.encryptAES((new Date().toISOString()),derivedOrganizationKey,ivString),
 
+      ).then(response=>{
+        this.creatingInProcess = false;
+        if(response.success){
+          let offcanvas = Offcanvas.getOrCreateInstance("#createNewOrgOffcanvas");
+          offcanvas.hide();
         }
       })
     }
--- frontend/gateway/WorkflowGateway.ts
+++ frontend/gateway/WorkflowGateway.ts
@@ -16,22 +16,24 @@ class WorkflowGateway extends Requests{
     public createNewOrg(
         name:string,
         genericId:string,
-        publicFLNames:boolean,
+        publicFLNames:string,
         iv:string,
         salt:string,
         encryptedPassword:string,
         publicKey:string,
         encryptedPrivateKey:string,
+        encryptedCreatedAt:string,
     ):Promise<any>{
         const formData = new FormData();
         formData.append("name",name);
         formData.append("genericId",genericId);
-        formData.append("publicFLNames",publicFLNames?"1":"0");
+        formData.append("publicFLNames",publicFLNames);
         formData.append("iv",iv);
         formData.append("salt",salt);
         formData.append("encryptedPassword",encryptedPassword);
         formData.append("publicKey",publicKey);
         formData.append("encryptedPrivateKey",encryptedPrivateKey);
+        formData.append("encryptedCreatedAt",encryptedCreatedAt);
         return this.executePost("/api/workflow/createOrg",formData);
     }
 }
