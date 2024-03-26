86fc0f4ccbd18d93a851097f47096fe064e1fa90
vlad <vladviolentiy@gmail.com>
2024-03-26 13:56:50 +0300

    add bases for notification service

---
+++ backend/Notifications/Controller.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Flow\Notifications;
+
+class Controller
+{
+
+}
---
+++ backend/Notifications/Storage/Storage.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Flow\Notifications\Storage;
+
+
+use VladViolentiy\VivaFramework\Databases\Mysqli;
+
+class Storage extends Mysqli
+{
+
+}
